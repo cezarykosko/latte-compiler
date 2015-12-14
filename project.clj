b0bc55a3e81d2aaa510fffceb4dca97135fd1e20
Cezary Kosko <koskocezarym@gmail.com>
2015-12-14 16:02:47 +0100

    space much

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A clojure-based compiler of Latte programming language"
   :url "http://github.com/cezarykosko/latte-compiler"
   :main latte-compiler.core
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [instaparse "1.4.1"]])
